e5b8e087d1bb020a8efb9277b70fb052effe64e3
Marc Delalonde <marc@aircall.io>
2016-02-05 18:26:55 +0100

    Add webpack-dev-server instead of browser-sync!

--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -1,45 +1,41 @@
 var loaders = require("./loaders");
-var BrowserSyncPlugin = require('browser-sync-webpack-plugin');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var webpack = require('webpack');
 module.exports = {
-    entry: ['./src/index.ts'],
-    output: {
-        filename: 'build.js',
-        path: 'dist'
-    },
-    resolve: {
-        root: __dirname,
-        extensions: ['', '.ts', '.js', '.json']
-    },
-    resolveLoader: {
-        modulesDirectories: ["node_modules"]
-    },
-    devtool: "source-map",
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/index.html',
-            inject: 'body',
-            hash: true
-        }),
-        new BrowserSyncPlugin({
-            host: 'localhost',
-            port: 8080,
-            server: {
-                baseDir: 'dist'
-            },
-            ui: false,
-            online: false,
-            notify: false
-        }),
-        new webpack.ProvidePlugin({
-            $: 'jquery',
-            jQuery: 'jquery',
-            'window.jQuery': 'jquery',
-            'window.jquery': 'jquery'
-        })
-    ],
-    module:{
-        loaders: loaders
-    }
+  entry: [
+    //'webpack-dev-server/client?http://localhost:8080', 
+    //'webpack/hot/only-dev-server',
+    './src/index.ts'
+  ],
+  output: {
+    filename: 'build.js',
+    path: 'dist'
+  },
+  resolve: {
+    root: __dirname,
+    extensions: ['', '.ts', '.js', '.json']
+  },
+  resolveLoader: {
+    modulesDirectories: ["node_modules"]
+  },
+  devtool: "source-map",
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      inject: 'body',
+      hash: true
+    }),
+    //new webpack.HotModuleReplacementPlugin(),
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+      'window.jQuery': 'jquery',
+      'window.jquery': 'jquery'
+    }),
+    new ExtractTextPlugin("build.css")
+  ],
+  module:{
+    loaders: loaders
+  }
 };
